cdf46035b0c6fb8f8b29b747542c06186692214f
Lispereh <61993769+Lispereh@users.noreply.github.com>
2024-11-15 23:48:55 +0300

    Add files via upload

---
+++ HW 1 ADO.NET/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = null;
+            connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;
+                                           Initial Catalog=VegetablesAndFruits;
+                                           Integrated Security=True;Connect Timeout=30;
+                                           Encrypt=False;");
+
+
+            try
+            {
+                connection.Open();
+
+                if (connection.State == ConnectionState.Open)
+                    Console.WriteLine("Подключение к БД успешно");
+
+                Repository repository = new Repository(connection);
+                
+                Console.WriteLine("\n---------- Отображение всей информации из таблицы с овощами и фруктами ----------");
+                Console.WriteLine(repository.GetAll());
+
+                Console.WriteLine("\n---------- Отображение всех названий овощей и фруктов ----------");
+                Console.WriteLine(repository.GetName());
+
+                Console.WriteLine("\n---------- Отображение всех цветов ----------");
+                Console.WriteLine(repository.GetColor());
+
+                Console.WriteLine("\n---------- Показать максимальную калорийность ----------");
+                Console.WriteLine(repository.GetMaxCaloricity());
+
+                Console.WriteLine("\n---------- Показать минимальную калорийность ----------");
+                Console.WriteLine(repository.GetMinCaloricity());
+
+                Console.WriteLine("\n---------- Показать среднюю калорийность ----------");
+                Console.WriteLine(repository.GetAVGCaloricity());
+
+                Console.WriteLine("\n---------- Показать количество овощей ----------");
+                Console.WriteLine(repository.QuantityVegetables());
+
+                Console.WriteLine("\n---------- Показать количество фруктов ----------");
+                Console.WriteLine(repository.QuantityFruits());
+
+                Console.WriteLine("\n---------- Показать количество фруктов и овощей зелёного цвета ----------");
+                Console.WriteLine(repository.QuantityFVSpecifiedColor("Зелёный"));
+
+                Console.WriteLine("\n---------- Показать количество фруктов и овощей каждого цвета ----------");
+                Console.WriteLine(repository.QuantityFVAllColors());
+
+                Console.WriteLine("\n---------- Показать овощи и фрукты с калорийностью ниже 20 ----------");
+                Console.WriteLine(repository.FVMinCaloricity(20));
+
+                Console.WriteLine("\n---------- Показать овощи и фрукты с калорийностью выше 20 ----------");
+                Console.WriteLine(repository.FVMaxCaloricity(20));
+
+                Console.WriteLine("\n---------- Показать овощи и фрукты с калорийностью в диапазоне от 10 до 30 ----------");
+                Console.WriteLine(repository.FVRangeCaloricity(10, 30));
+
+                Console.WriteLine("\n---------- Показать овощи и фрукты, у которых цвет желтый или красный ----------");
+                Console.WriteLine(repository.YellowOrRedColor());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW 1 ADO.NET/Repository.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Repository
+    {
+        private readonly SqlConnection _connection;
+
+        public Repository(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public string GetAll()
+        {
+            string allData = string.Empty;
+
+            SqlCommand cmd = new SqlCommand("GetALl", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while(reader.Read())
+                    allData += reader["Id"] + " " + reader["FoodName"] + " " + reader["FoodType"] + " " + reader["FoodColor"] + " " + reader["Сaloricity"] + '\n';
+
+                return allData;
+            }
+        }
+
+        public string GetName()
+        {
+            string allData = string.Empty;
+
+            SqlCommand cmd = new SqlCommand("GetFoodName", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                    allData += reader["FoodName"] + "\n";
+
+                return allData;
+            }
+        }
+
+        public string GetColor()
+        {
+            string allData = string.Empty;
+
+            SqlCommand cmd = new SqlCommand("GetFoodColor", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                    allData += reader["FoodColor"] + "\n";
+
+                return allData;
+            }
+        }
+
+        public decimal GetMaxCaloricity()
+        {
+            SqlCommand cmd = new SqlCommand("Procedure", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            return Convert.ToDecimal(cmd.ExecuteScalar());
+        }
+
+        public decimal GetMinCaloricity()
+        {
+            SqlCommand cmd = new SqlCommand("MinCaloricity", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            return Convert.ToDecimal(cmd.ExecuteScalar());
+        }
+
+        public decimal GetAVGCaloricity()
+        {
+            SqlCommand cmd = new SqlCommand("AVGCaloricity", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            return Convert.ToDecimal(cmd.ExecuteScalar());
+        }
+
+        public int QuantityVegetables()
+        {
+            SqlCommand cmd = new SqlCommand("QuantityV", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            return Convert.ToInt32(cmd.ExecuteScalar());
+        }
+
+        public int QuantityFruits()
+        {
+            SqlCommand cmd = new SqlCommand("QuantityF", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            return Convert.ToInt32(cmd.ExecuteScalar());
+        }
+
+        public int QuantityFVSpecifiedColor(string color)
+        {
+            SqlCommand cmd = new SqlCommand("QuantityFVSpecifiedColor", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter parameter = new SqlParameter("@color", SqlDbType.NVarChar);
+            parameter.Value = color;
+            cmd.Parameters.Add(parameter);
+
+            return Convert.ToInt32(cmd.ExecuteScalar());
+        }
+
+        public string QuantityFVAllColors()
+        {
+            string allData = string.Empty;
+            SqlCommand cmd = new SqlCommand("QuantityFVAllColors", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string foodColor = reader["FoodColor"].ToString();
+                    int quantity = (int)reader["Quantity"];
+                    allData += $"{foodColor}: {quantity}\n";
+                }
+
+                return allData;
+            }
+        }
+
+        public string FVMinCaloricity(decimal ccal)
+        {
+            string allData = string.Empty;
+            SqlCommand cmd = new SqlCommand("FVMinCaloricity", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter parameter = new SqlParameter("@ccal", SqlDbType.Decimal);
+            parameter.Value = ccal;
+            cmd.Parameters.Add(parameter);
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                    allData += reader["Id"] + " " + reader["FoodName"] + " " + reader["FoodType"] + " " + reader["FoodColor"] + " " + reader["Сaloricity"] + '\n';
+
+                return allData;
+            }
+        }
+
+        public string FVMaxCaloricity(decimal ccal)
+        {
+            string allData = string.Empty;
+            SqlCommand cmd = new SqlCommand("FVMaxCaloricity", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter parameter = new SqlParameter("@ccal", SqlDbType.Decimal);
+            parameter.Value = ccal;
+            cmd.Parameters.Add(parameter);
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                    allData += reader["Id"] + " " + reader["FoodName"] + " " + reader["FoodType"] + " " + reader["FoodColor"] + " " + reader["Сaloricity"] + '\n';
+
+                return allData;
+            }
+        }
+
+        public string FVRangeCaloricity(decimal min, decimal max)
+        {
+            string allData = string.Empty;
+            SqlCommand cmd = new SqlCommand("CaloricityRange", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter parameter = new SqlParameter("@min", SqlDbType.Decimal);
+            parameter.Value = min;
+            cmd.Parameters.Add(parameter);
+
+            SqlParameter parameter1 = new SqlParameter("@max", SqlDbType.Decimal);
+            parameter1.Value = max;
+            cmd.Parameters.Add(parameter1);
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                    allData += reader["Id"] + " " + reader["FoodName"] + " " + reader["FoodType"] + " " + reader["FoodColor"] + " " + reader["Сaloricity"] + '\n';
+
+                return allData;
+            }
+        }
+
+        public string YellowOrRedColor()
+        {
+            string allData = string.Empty;
+            SqlCommand cmd = new SqlCommand("YellowOrRedColor", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                    allData += reader["Id"] + " " + reader["FoodName"] + " " + reader["FoodType"] + " " + reader["FoodColor"] + " " + reader["Сaloricity"] + '\n';
+
+                return allData;
+            }
+        }
+    }
+}
